d94f594e1130fc94d781aa32a96735401eb22fca
Yuji Saikai <saikai@me.com>
2022-08-07 15:50:42 +1000

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,264 @@
+﻿namespace Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.IO;
+    using Models.Core.Run;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>Class to hold a static main entry point.</summary>
+    public class Program
+    {
+        /// <summary>policy network</summary>
+        public static PolicyNet policyNet;
+        /// <summary> A set of irrigation amounts </summary>
+        public static float[] amounts = { 0f, 10f, 20f, 30f, 40f };
+        /// <summary></summary>
+        public static float yield;
+        /// <summary></summary>
+        public static float priceOut = 0.25f;
+        /// <summary></summary>
+        public static float priceWater = 0.6f;
+        /// <summary></summary>
+        public static int episode;
+        /// <summary></summary>
+        public static List<float> days;
+        /// <summary></summary>
+        public static List<float> stages;
+        /// <summary></summary>
+        public static List<float> fws;
+        /// <summary></summary>
+        public static List<float> lais;
+        /// <summary></summary>
+        public static List<float> esw0s;
+        /// <summary></summary>
+        public static List<float> esw1s;
+        /// <summary></summary>
+        public static List<float> esw2s;
+        /// <summary></summary>
+        public static List<float> esw3s;
+        /// <summary></summary>
+        public static List<float> esw4s;
+        /// <summary></summary>
+        public static List<float> esw5s;
+        /// <summary></summary>
+        public static List<float> esw6s;
+        /// <summary></summary>
+        public static List<float> cuIrrigs;
+        /// <summary></summary>
+        public static List<float> cuRains;
+        /// <summary></summary>
+        public static List<int> actions;
+        /// <summary></summary>
+        public static List<float[]> probs;
+
+        static Dictionary<int, List<int>> actionsMax = new Dictionary<int, List<int>>();
+        static Dictionary<int, float> profitMax = new Dictionary<int, float>();
+
+        /// <summary></summary>
+        public static void Main(string[] args)
+        {
+            // Parameters
+            float lr;
+            int n_episodes;
+            int rndSeed;
+            try // console inputs
+            {
+                lr = (float)Math.Pow(10, Int32.Parse(args[0]));
+                n_episodes = Int32.Parse(args[1]);
+                rndSeed = Int32.Parse(args[2]);
+            }
+            catch // default
+            {
+                lr = 1e-7f;
+                n_episodes = 1000;
+                rndSeed = (int)DateTime.Now.Ticks & 0x0000FFFF;
+            }
+
+            int order = 100; // Order of MA
+            int[] n_hidden = { 400, 600, 800, 600, 400 };
+            string location = "Goond";
+            int yearStart = 1981;
+            int yearEnd = 2020;
+            var yearsExcluded = new List<int> { };
+
+            int year;
+            for (year = yearStart; year <= yearEnd; year++)
+            {
+                if (!yearsExcluded.Contains(year))
+                {
+                    profitMax.Add(year, 0f);
+                    actionsMax.Add(year, new List<int>());
+                }
+            }
+
+            // Variables
+            float profit;
+            float ma = 0f; // moving average
+            float maMax = float.NegativeInfinity;
+            int episodeMax = 0;
+            Random rand = new Random(rndSeed);
+            policyNet = new PolicyNet(n_hidden, rand);
+            PolicyNet policyNetMax = new PolicyNet(n_hidden, rand);
+
+            // Relative path
+            string path = System.AppDomain.CurrentDomain.BaseDirectory;
+            path = System.IO.Path.GetDirectoryName(path);
+            path = System.IO.Path.GetDirectoryName(path); // two-level above
+
+            string fileNameAPSIM = path + $"\\Wheat.apsimx";
+
+            // Initial message
+            string msg = "\n";
+            msg += "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\n";
+            msg += "{0} episodes; lr={1}; seed={2}; H1-5={3}-{4}-{5}-{6}-{7}\n";
+            msg += "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~";
+            Console.WriteLine(msg, n_episodes, lr, rndSeed, n_hidden[0],
+                              n_hidden[1], n_hidden[2], n_hidden[3], n_hidden[4]);
+
+            //**********
+            // Learning
+            //**********
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            for (episode = 1; episode <= n_episodes; episode++)
+            {
+                stopWatch.Start();
+                Initialize();
+
+                // Except the last 10 years
+                do
+                    year = rand.Next(yearStart, yearEnd - 9);
+                while (yearsExcluded.Contains(year));
+
+                ProcessApsimFile(fileNameAPSIM, year, location);
+                var runner = new Runner(fileNameAPSIM);
+                runner.Run();
+
+                // Update
+                profit = priceOut * yield - priceWater * cuIrrigs[days.Count - 1];
+                if (profit > profitMax[year])
+                {
+                    profitMax[year] = profit;
+                    actionsMax[year] = actions;
+                }
+
+                float factor = new int[] { episode, order }.Min();
+                ma = (ma * (factor - 1) + profit) / factor;
+                if (ma > maMax)
+                {
+                    maMax = ma;
+                    episodeMax = episode;
+                    policyNetMax = policyNet.DeepCopy(); // Save the best policy
+                }
+                policyNet.Update(lr, episode);
+
+                // Progress
+                if (episode % 500 == 0)
+                {
+                    stopWatch.Stop();
+                    TimeSpan ts = stopWatch.Elapsed;
+                    Console.WriteLine("{0,5:D1}: {1,4:F0} / {2:0} (at {3}) {4:00}:{5:00}",
+                                      episode, ma, maMax, episodeMax, ts.Hours, ts.Minutes);
+                }
+            }
+
+            //**********
+            // Testing
+            //**********
+            policyNet = policyNetMax; // Demonstrate the best policy
+
+            string dirName = path + $"\\re{DateTime.Now.ToString("yyyyMMddHHmmss")}";
+            if (!Directory.Exists(dirName)) Directory.CreateDirectory(dirName);
+
+            // Include the training years
+            for (year = yearStart; year <= yearEnd; year++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    Initialize();
+                    ProcessApsimFile(fileNameAPSIM, year, location);
+                    var runner = new Runner(fileNameAPSIM);
+                    runner.Run();
+
+                    profit = priceOut * yield - priceWater * cuIrrigs[days.Count - 1];
+                    using (var file = new StreamWriter(dirName + $"\\{year}_{j}.csv"))
+                    {
+                        file.WriteLine($"{profit}");
+                        file.WriteLine($"{yield}");
+                        file.WriteLine($"{cuIrrigs[days.Count - 1]}");
+                        file.WriteLine($"{rndSeed}");
+                        file.WriteLine($"{n_episodes}");
+                        file.WriteLine($"{n_hidden[0]}-{n_hidden[1]}-{n_hidden[2]}-{n_hidden[3]}-{n_hidden[4]}");
+                        file.WriteLine("");
+                        file.WriteLine("Day, Stage,   FW,  LAI, ESW0, ESW1, ESW2, ESW3, ESW4, ESW5, ESW6, " +
+                                       "cuIrrig, cuRain, Action, p(0), p(1), p(2), p(3), p(4)");
+                        for (int i = 0; i < days.Count; i++)
+                        {
+                            file.Write("{0:0}, {1,5:F1}, {2,4:F2}, {3,4:F2}, {4,4:F0}, {5,4:F0}, {6,4:F0}, " +
+                                       "{7,4:F0}, {8,4:F0}, {9,4:F0}, {10,4:F0}, {11,7:F0}, {12,6:F0}, {13,6:D1}, ",
+                                           days[i],
+                                           stages[i],
+                                           fws[i],
+                                           lais[i],
+                                           esw0s[i],
+                                           esw1s[i],
+                                           esw2s[i],
+                                           esw3s[i],
+                                           esw4s[i],
+                                           esw5s[i],
+                                           esw6s[i],
+                                           cuIrrigs[i],
+                                           cuRains[i],
+                                           actions[i]);
+                            file.WriteLine("{0:0.00}, {1:0.00}, {2:0.00}, {3:0.00}, {4:0.00}",
+                                           probs[i][0],
+                                           probs[i][1],
+                                           probs[i][2],
+                                           probs[i][3],
+                                           probs[i][4]);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        static void Initialize()
+        {
+            yield = 0f;
+            days = new List<float>();
+            stages = new List<float>();
+            fws = new List<float>();
+            lais = new List<float>();
+            esw0s = new List<float>();
+            esw1s = new List<float>();
+            esw2s = new List<float>();
+            esw3s = new List<float>();
+            esw4s = new List<float>();
+            esw5s = new List<float>();
+            esw6s = new List<float>();
+            cuIrrigs = new List<float>();
+            cuRains = new List<float>();
+            actions = new List<int>();
+            probs = new List<float[]>();
+        }
+
+
+        // Process .apsimx file as JSON file (https://stackoverflow.com/a/56027969)
+        static void ProcessApsimFile(string fileNameAPSIM, int year, string location)
+        {
+            string jsonString = File.ReadAllText(fileNameAPSIM);
+            JObject jObject = JsonConvert.DeserializeObject(jsonString) as JObject;
+            JToken jtStart = jObject.SelectToken("Children[0].Children[0].Start");
+            jtStart.Replace(year.ToString() + "-01-01T00:00:00");
+            JToken jtEnd = jObject.SelectToken("Children[0].Children[0].End");
+            jtEnd.Replace(year.ToString() + "-12-31T00:00:00");
+            JToken jtLoc = jObject.SelectToken("Children[0].Children[2].FileName");
+            jtLoc.Replace(".\\" + location + ".met");
+            string updatedJsonString = jObject.ToString();
+            File.WriteAllText(fileNameAPSIM, updatedJsonString);
+        }
+    }
+}
---
+++ PolicyNet.cs
@@ -0,0 +1,177 @@
+﻿using NumSharp;
+using System;
+using System.Data;
+using System.Linq;
+using Tensorflow;
+using Tensorflow.Keras.Optimizers;
+using static Tensorflow.Binding;
+
+namespace Models
+{
+    /// <summary></summary>
+    public class PolicyNet
+    {
+        int n_features = 9;
+        int n_classes = 5;
+
+        // For state var standardization
+        NDArray means = np.array(new float[] { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f });
+        NDArray stds = np.array(new float[] { 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f });
+
+        IVariableV1 h1, h2, h3, h4, h5, wout, b1, b2, b3, b4, b5, bout;
+        IVariableV1[] trainable_variables;
+        OptimizerV2 optimizer;
+        Random rand;
+
+        /// <summary></summary>
+        public PolicyNet DeepCopy()
+        {
+            PolicyNet clonedNet = (PolicyNet)this.MemberwiseClone();
+            clonedNet.h1 = tf.Variable(h1);
+            clonedNet.h2 = tf.Variable(h2);
+            clonedNet.h3 = tf.Variable(h3);
+            clonedNet.h4 = tf.Variable(h4);
+            clonedNet.h5 = tf.Variable(h5);
+            clonedNet.wout = tf.Variable(wout);
+            clonedNet.b1 = tf.Variable(b1);
+            clonedNet.b2 = tf.Variable(b2);
+            clonedNet.b3 = tf.Variable(b3);
+            clonedNet.b4 = tf.Variable(b4);
+            clonedNet.b5 = tf.Variable(b5);
+            clonedNet.bout = tf.Variable(bout);
+            clonedNet.means = (NDArray)means.Clone();
+            clonedNet.stds = (NDArray)stds.Clone();
+
+            return clonedNet;
+        }
+
+        /// <summary></summary>
+        public PolicyNet(int[] n_hidden, Random rnd)
+        {
+            tf.enable_eager_execution();
+
+            rand = rnd;
+            var ini = tf.initializers.random_normal_initializer(seed: rand.Next());
+            h1 = tf.Variable(ini.Apply(new InitializerArgs((n_features, n_hidden[0]))));
+            h2 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[0], n_hidden[1]))));
+            h3 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[1], n_hidden[2]))));
+            h4 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[2], n_hidden[3]))));
+            h5 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[3], n_hidden[4]))));
+            wout = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[4], n_classes))));
+            b1 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[0]))));
+            b2 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[1]))));
+            b3 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[2]))));
+            b4 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[3]))));
+            b5 = tf.Variable(ini.Apply(new InitializerArgs((n_hidden[4]))));
+            bout = tf.Variable(ini.Apply(new InitializerArgs((n_classes))));
+
+            trainable_variables = new IVariableV1[] { h1, h2, h3, h4, h5, wout,
+                                                      b1, b2, b3, b4, b5, bout };
+        }
+
+
+        /// <summary></summary>
+        public void Update(float learning_rate, int episode)
+        {
+            NDArray S = np.hstack(np.array(Program.stages).reshape(-1, 1),
+                                  np.array(Program.lais).reshape(-1, 1),
+                                  np.array(Program.esw0s).reshape(-1, 1),
+                                  np.array(Program.esw1s).reshape(-1, 1),
+                                  np.array(Program.esw2s).reshape(-1, 1),
+                                  np.array(Program.esw3s).reshape(-1, 1),
+                                  np.array(Program.esw4s).reshape(-1, 1),
+                                  np.array(Program.cuIrrigs).reshape(-1, 1),
+                                  np.array(Program.cuRains).reshape(-1, 1));
+
+            NDArray meansOld = (NDArray)means.Clone();
+            NDArray stdsOld = (NDArray)stds.Clone();
+
+            float factor = new int[] { episode, 100 }.Min();
+            for (int i = 0; i < n_features; i++)
+            {
+                var column = S[$":,{i}"];
+
+                means[i] = (column.mean() + meansOld[i] * (factor - 1)) / factor; // new mean
+                stds[i] = (column.std() + stdsOld[i] * (factor - 1)) / factor; // new std
+
+                // standardised states
+                S[$":,{i}"] = (column - means[i]);
+                //// Divide it only if not too small
+                if (stds[i] > 1e-8f)
+                    S[$":,{i}"] = S[$":,{i}"] / stds[i];
+            }
+
+            // A for masking, i.e. multiplying probabilities for unchosen actions by 0
+            var A = tf.one_hot(np.array(Program.actions), n_classes);
+
+            // To make use of masking by A, G has identical column entries at each row
+            int n_days = Program.days.Count; // Same size in all the state variables
+            float g = Program.priceOut * Program.yield; // revenue
+            var G = np.ones((n_days, n_classes), dtype: np.float32); // initialization
+            for (int i = n_days - 1; i >= 0; i--) // start from the harvest day
+            {
+                int a = Program.actions[i];
+                g -= Program.priceWater * Program.amounts[a]; // water cost
+                G[i] = G[i] * g; // each row (i) has the identical entry (g)
+            }
+
+            // Gradient ascent
+            optimizer = tf.optimizers.SGD(learning_rate);
+            using (var tape = tf.GradientTape())
+            {
+                var PROBS = Predict(S);
+                // Multiplying by A, each row has only 1 nonzero entry
+                var loss = tf.reduce_sum(-tf.math.log(PROBS) * A * G);
+                var gradients = tape.gradient(loss, trainable_variables);
+                optimizer.apply_gradients(zip(gradients,
+                                trainable_variables.Select(x => x as ResourceVariable)));
+            }
+        }
+
+
+        /// <summary>Called by the IrrigationPolicy script</summary>
+        public Tuple<int, float[]> Action(float stage, float fw, float lai, float[] esw, float cuIrrig, float cuRain)
+        {
+            var x = new NDArray(new float[] { stage, lai, esw[0], esw[1], esw[2], esw[3], esw[4], cuIrrig, cuRain });
+
+            // Standardization
+            for (int i = 0; i < n_features; i++)
+            {
+                x[i] = x[i] - means[i];
+                // Divide it only if not too small
+                if (stds[i] > 1e-8f)
+                    x[i] = x[i] / stds[i];
+            }
+
+            float[] probs = Predict(x.reshape(1, n_features)).ToArray<float>();
+            float r = (float)rand.NextDouble();
+            float sum = 0f;
+            for (int a = 0; a < n_classes; a++)
+            {
+                sum += probs[a];
+                if (sum >= r)
+                    return Tuple.Create(a, probs);
+            }
+            // This is never reached but the compiler requires.
+            return Tuple.Create(n_classes - 1, probs);
+        }
+
+
+        /// <summary></summary>
+        public Tensor Predict(Tensor x)
+        {
+            var layer_1 = tf.add(tf.matmul(x, h1.AsTensor()), b1.AsTensor());
+            layer_1 = tf.nn.relu(layer_1);
+            var layer_2 = tf.add(tf.matmul(layer_1, h2.AsTensor()), b2.AsTensor());
+            layer_2 = tf.nn.relu(layer_2);
+            var layer_3 = tf.add(tf.matmul(layer_2, h3.AsTensor()), b3.AsTensor());
+            layer_3 = tf.nn.relu(layer_3);
+            var layer_4 = tf.add(tf.matmul(layer_3, h4.AsTensor()), b4.AsTensor());
+            layer_4 = tf.nn.relu(layer_4);
+            var layer_5 = tf.add(tf.matmul(layer_4, h5.AsTensor()), b5.AsTensor());
+            layer_5 = tf.nn.relu(layer_5);
+            var out_layer = tf.matmul(layer_5, wout.AsTensor()) + bout.AsTensor();
+            return tf.nn.softmax(out_layer);
+        }
+    }
+}
